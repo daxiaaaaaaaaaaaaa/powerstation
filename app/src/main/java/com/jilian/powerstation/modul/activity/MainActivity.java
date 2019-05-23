d719b0717dddd19543c10d354850f3b3cd934226
247836277@qq.com <daxiaa11>
2019-05-23 22:34:35 +0800

    asdasda

--- app/src/main/java/com/jilian/powerstation/modul/activity/MainActivity.java
+++ app/src/main/java/com/jilian/powerstation/modul/activity/MainActivity.java
@@ -1,8 +1,15 @@
 package com.jilian.powerstation.modul.activity;
 
+import com.jilian.powerstation.MyApplication;
+
 public class MainActivity extends BaseMainActivity {
     @Override
     protected void createViewModel() {
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MyApplication.clearSpecifyActivities(new Class[]{WelcomeActivity.class});
     }
 }
---
+++ app/src/main/java/com/jilian/powerstation/modul/activity/WelcomeActivity.java
@@ -0,0 +1,34 @@
+package com.jilian.powerstation.modul.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.jilian.powerstation.MyApplication;
+
+public class WelcomeActivity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,MainActivity.class));
+        MyApplication.addActivity(this);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MyApplication.removeActivity(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
